c7897b7a7a8b0ba0e9261f554abd7518060c0f7c
retacg <62267774+retacg@users.noreply.github.com>
2023-01-06 22:00:17 +0000

    add customers query v2

---
+++ customers.sql
@@ -0,0 +1,9 @@
+select c.id
+,c.name
+,c.email
+,min(o.created_at) as first_order_at
+,count(distinct o.id) as number_of_orders
+ from `analytics-engineers-club.coffee_shop.customers` c
+
+left join `analytics-engineers-club.coffee_shop.orders` o on c.id=o.customer_id
+group by 1,2,3
